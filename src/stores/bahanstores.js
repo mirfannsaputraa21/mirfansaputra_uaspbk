107144c1b70c05026b584a4b35fa33797b04bc35
mirfannsaputraa21 <m.irfansaputra0089@gmail.com>
2025-07-04 23:47:58 +0700

    api json

--- src/stores/bahanstores.js
+++ src/stores/bahanstores.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-const API_URL = 'https://irfan-json-server.glitch.me/bahan';
+const API_URL = 'https://irfan-uas-prak.glitch.me/bahan';
 
 export const useBahanStore = defineStore('bahan', {
   state: () => ({
